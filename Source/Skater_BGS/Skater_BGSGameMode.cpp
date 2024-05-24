b4206fe8a24374e63fdf1f740ce65875799a5c8f
JuanIgnacioTubio <juanignaciotubio2007@gmail.com>
2024-05-24 00:04:36 -0300

    Implemented basis for HUD

--- Source/Skater_BGS/Skater_BGSGameMode.cpp
+++ Source/Skater_BGS/Skater_BGSGameMode.cpp
@@ -7,7 +7,7 @@
 ASkater_BGSGameMode::ASkater_BGSGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Mechanics/Player/BP_Player_Skater"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Systems/Player/BP_Player_Skater"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
